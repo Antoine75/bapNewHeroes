a93c08e7cc809d8fc0359a534b479f96d3718f64
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-01-03 16:54:22 +0100

    :tada: initial commit

---
+++ template-parts/content-login.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bapnewheroes
+ */
+
+?>
+
+<section class="login-wrapper">
+    <div class="form-login-wrapper">
+        <h2 class="connexion">Connexion</h2>
+        <div class="user-input-form">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Adresse E-Mail</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Adresse E-Mail">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Mot de Passe</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Mot de Passe">
+            </div>
+
+            <a href="#" class="password-failsafe">Mot de passe ou identifiant oubliés ?</a>
+        </div>
+        <div class="user-control">
+            <div class="form-check">
+
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">Rester Connecté</label>
+
+            </div>
+
+            <button type="button" class="btn btn-warning test" >Se connecter</button>
+            <span class="warning-membership">Vous n'êtes pas membre ?</span>
+            <button type="button" class="btn btn-outline-warning border">Créer un compte</button>
+        </div>
+    </div>
+</section>
